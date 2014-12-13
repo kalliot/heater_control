46793f714a118fc35e60b0f06b37d6c840846120
Teppo Kallio <teppo.ilari.kallio@gmail.com>
2014-12-13 12:06:56 +0200

    [feat] added more commands to cmdparser. Biggest change is showsetup command. With it you can see what is the current setup, and edit/push it back.

--- cmdparser.ino
+++ cmdparser.ino
@@ -1,4 +1,5 @@
 #include <Ethernet.h>
+#include <stdlib.h>
 #include "eepromsetup.h"
 
 struct ipcmd {
@@ -17,36 +18,113 @@ static struct ipcmd commands[] = {
   {"m2xfeed",  1, ipm2xfeed},
   {"m2xkey",   1, ipm2xkey},
   {"savesetup",0, ipsavesetup},
+  {"showsetup",0, ipshowsetup},
   {"",0,NULL}
 };
 
+static int search_ad(char *name)
+{
+  for (int i=0;i < sizeof(adArr) / sizeof(struct ad);i++) {
+    if (!strcmp(adArr[i].name,name))
+      return i;
+  }
+  return -1;
+}
+
+static void reply2remote(EthernetClient c,char *cmd,char *ret,char *msg)
+{
+    c.write(cmd);
+    c.write(ret);
+    c.write(msg);
+    c.write("\n");
+}
 
 static void ipadcalibr(EthernetClient client,int argc,char *argv[])
 {
-  for (int i=0;i < sizeof(adArr) / sizeof(struct ad);i++) {
-    if (!strcmp(adArr[i].name,argv[1])) {
-      if (!strcmp(argv[2],"min")) {
-	return;
-      }
-      else if (!strcmp(argv[2],"max")) {
-	return;
-      }
-      else if (!strcmp(argv[2],"diff")) {
-	return;
-      }
-      else {
-	client.write(argv[0]);
-	client.write(":ERR:UNKNOWN PARAMETER:");
-	client.write(argv[2]);
-	client.write("\n");
-      }
-      return;
-    }
+  int adindex=search_ad(argv[1]);
+  if (adindex==-1) {
+    reply2remote(client,argv[0],":ERR:UNKNOWN CHANNEL:",argv[1]);
+    return;
   }
-  client.write(argv[0]);
-  client.write(":ERR:UNKNOWN CHANNEL:");
-  client.write(argv[1]);
+  if (!strcmp(argv[2],"min")) {
+    float v=atof(argv[3]);
+    adArr[adindex].mincal.analog=v;
+    if (argc==4)  // use current digital value;
+      adArr[adindex].mincal.digital=adArr[adindex].prev.digital;
+    if (argc==5) // user what we got from remote
+      adArr[adindex].mincal.digital=atoi(argv[4]);
+    reply2remote(client,argv[0],":OK:",argv[1]);
+    return;
+  }
+  else if (!strcmp(argv[2],"max")) {
+    float v=atof(argv[3]);
+    adArr[adindex].maxcal.analog=v;
+    if (argc==4)  // use current digital value;
+      adArr[adindex].maxcal.digital=adArr[adindex].prev.digital;
+    if (argc==5) // user what we got from remote
+      adArr[adindex].maxcal.digital=atoi(argv[4]);
+    reply2remote(client,argv[0],":OK:",argv[1]);
+    return;
+  }
+  else if (!strcmp(argv[2],"diff")) {
+    return;
+  }
+  else {
+    reply2remote(client,argv[0],":ERR:UNKNOWN PARAM:",argv[2]);
+  }
+}
+
+// arm float to char* conversion is a bit complicated, so
+// lets make a function for it.
+
+static char *f2str(float val)
+{
+  static char str[12];
+  String tmp;
+
+  tmp=String(dtostrf(val,12,2,str));
+  tmp.trim();
+  tmp.toCharArray(str,12);
+  return str;
+}
+
+static void ipshowsetup(EthernetClient client,int argc,char *argv[])
+{
+  char str[12];
+
+  client.write("m2xfeed,");
+  client.write(eepromsetup.m2xfeed);
+  client.write("\nm2key,");
+  client.write(eepromsetup.m2xkey);
   client.write("\n");
+
+  for (int i=0;i < sizeof(adArr) / sizeof(struct ad);i++) {
+    client.write("adcalibr,");
+    client.write(adArr[i].name);
+    client.write(",min,");
+    client.write(f2str(adArr[i].mincal.analog));
+
+    client.write(",");
+    client.write(itoa(adArr[i].mincal.digital,str,10));
+    client.write("\n");
+
+    client.write("adcalibr,");
+    client.write(adArr[i].name);
+    client.write(",max,");
+    client.write(f2str(adArr[i].maxcal.analog));
+
+    client.write(",");
+    client.write(itoa(adArr[i].maxcal.digital,str,10));
+    client.write("\n");
+
+    client.write("adcalibr,");
+    client.write(adArr[i].name);
+    client.write(",diff,");
+    client.write(f2str(adArr[i].diff.analog));
+
+    client.write("\n");
+  }
+  reply2remote(client,argv[0],":OK","");
   return;
 }
 
@@ -78,37 +156,33 @@ static void ipsetio(EthernetClient client,int argc,char *argv[])
   int port=atoi(argv[1]);
   int value=atoi(argv[2]);
 
-  if (port > 11 && port < 14)
+  if (port > 11 && port < 14) {
     digitalWrite(port,value);
-  client.write(argv[0]);
-  client.write(":OK\n");
+    reply2remote(client,argv[0],":OK","");
+    return;
+  }
+  reply2remote(client,argv[0],":ERR:","OUT OF RANGE");
 }
 
 
 static void ipevents(EthernetClient c,int argc,char *argv[])
 {
-  if (!strcmp(argv[1],"START")) {
+  if (!strcmp(argv[1],"start")) {
       addEventListener(c);
-      c.write(argv[0]);
-      c.write(":OK:START\n");
+      reply2remote(c,argv[0],":OK","START");
       return;
   }
-  if (!strcmp(argv[1],"STOP")) {
+  if (!strcmp(argv[1],"stop")) {
       removeEventListener();
-      c.write(argv[0]);
-      c.write(":OK:STOP\n");
+      reply2remote(c,argv[0],":OK","STOP");
       return;
   }
-  c.write(argv[0]);
-  c.write(":ERR:UNKNOWN OPTION:");
-  c.write(argv[1]);
-  c.write("\n");
+  reply2remote(c,argv[0],":ERR:UNKNOWN OPTION",argv[1]);
 }
 
 static void ipclose(EthernetClient c,int argc,char *argv[])
 {   
-  c.write(argv[0]);
-  c.write(":OK\n");
+  reply2remote(c,argv[0],":OK","");
   removeEventListener();
   c.stop();
 }
@@ -141,8 +215,7 @@ void cmdParse(EthernetClient c,char *command)
   for (i=0;commands[i].handler!=NULL;i++) {
     if (!strcmp(wptr[0],commands[i].name)) {
       if (wcnt < commands[i].minparams) {
-        c.write(wptr[0]);
-	c.write(":ERR:NOT ENOUGH PARAMS\n");
+	reply2remote(c,wptr[0],":ERR:","NOT ENOUGH PARAMS");
 	Serial.println(": not enough params");
         return;
       }
@@ -152,6 +225,5 @@ void cmdParse(EthernetClient c,char *command)
       }
     }
   }
-  c.write(wptr[0]);
-  c.write(":ERR:COMMAND NOT FOUND\n");  
+  reply2remote(c,wptr[0],":ERR:","COMMAND NOT FOUND");
 }
